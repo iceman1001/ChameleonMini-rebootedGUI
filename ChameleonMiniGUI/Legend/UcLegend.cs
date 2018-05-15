5e3782b41762444daf39a1eedb9fac043cf70902
iceman1001 <iceman@iuse.se>
2018-05-15 17:37:37 +0200

    chg:  adjustments to code

--- ChameleonMiniGUI/Legend/UcLegend.cs
+++ ChameleonMiniGUI/Legend/UcLegend.cs
@@ -1,12 +1,6 @@
 using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Drawing;
-using System.Data;
-using System.Linq;
-using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace ChameleonMiniGUI

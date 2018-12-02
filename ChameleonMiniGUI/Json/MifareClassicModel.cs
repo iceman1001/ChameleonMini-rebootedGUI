80836ee0059ac8e642ed9feefdb8e836aa28716f
kgame <kgamecarter@gmail.com>
2018-12-03 01:48:28 +0800

    completed json member

--- ChameleonMiniGUI/Json/MifareClassicModel.cs
+++ ChameleonMiniGUI/Json/MifareClassicModel.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.Serialization;
@@ -18,6 +19,38 @@ namespace ChameleonMiniGUI.Json
 
         [DataMember(Name = "blocks")]
         public byte[][] Blocks { get; set; }
+        
+        [DataMember]
+        public MifareClassicCardInfo Card
+        {
+            get => new MifareClassicCardInfo(this);
+            set { }
+        }
+
+        [DataMember]
+        public Dictionary<string, MifareClassicSectorKey> SectorKeys
+        {
+            get => Enumerable
+                .Range(0, Blocks.Length / 4)
+                .ToDictionary(i => i.ToString(), i => new MifareClassicSectorKey(this, i));
+            set { }
+        }
+
+        public static byte[] StringToByteArray(string hex)
+            => Enumerable.Range(0, hex.Length)
+                             .Where(x => x % 2 == 0)
+                             .Select(x => Convert.ToByte(hex.Substring(x, 2), 16))
+                             .ToArray();
+
+        public static string ByteArrayToString(IEnumerable<byte> bytes)
+            => string.Join("", bytes.Select(b => b.ToString("X2")));
+        
+        public static byte[][] ToNestedByteArray(byte[] data)
+        {
+            return Enumerable.Range(0, data.Length / 16)
+                .Select(i => data.Skip(i * 16).Take(16).ToArray())
+                .ToArray();
+        }
 
         public byte[] ToByteArray()
             => Blocks.SelectMany(bytes => bytes).ToArray();

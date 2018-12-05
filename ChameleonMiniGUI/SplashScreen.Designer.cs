b9b8c1f194171ad72bc9dd3505feaa707d88b83e
Vrumfondel <carsten@schaudel.de>
2018-12-05 09:54:34 +0000

    Add files via WebInterface

---
+++ ChameleonMiniGUI/SplashScreen.Designer.cs
@@ -0,0 +1,68 @@
+﻿namespace ChameleonMiniGUI
+{
+    partial class SplashScreen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SplashScreen));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::ChameleonMiniGUI.Properties.Resources.chameleon;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(9, 11);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(310, 310);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // SplashScreen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(331, 323);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "SplashScreen";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "SplashScreen";
+            this.TransparencyKey = System.Drawing.Color.White;
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
---
+++ ChameleonMiniGUI/SplashScreen.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ChameleonMiniGUI
+{
+    public partial class SplashScreen : Form
+    {
+        public SplashScreen()
+        {
+            InitializeComponent();
+        }
+        //Delegate for cross thread call to close
+        private delegate void CloseDelegate();
+
+        //The type of form to be displayed as the splash screen.
+        private static SplashScreen splashScreen;
+
+        static public void ShowSplashScreen()
+        {
+            // Make sure it is only launched once.
+
+            if (splashScreen != null)
+                return;
+            Thread thread = new Thread(new ThreadStart(SplashScreen.ShowForm));
+            thread.IsBackground = true;
+            thread.SetApartmentState(ApartmentState.STA);
+            thread.Start();
+        }
+
+        static private void ShowForm()
+        {
+            splashScreen = new SplashScreen();
+            Application.Run(splashScreen);
+        }
+
+        static public void CloseForm()
+        {
+            splashScreen.Invoke(new CloseDelegate(SplashScreen.CloseFormInternal));
+        }
+
+        static private void CloseFormInternal()
+        {
+            splashScreen.Close();
+            splashScreen = null;
+        }
+    }
+}

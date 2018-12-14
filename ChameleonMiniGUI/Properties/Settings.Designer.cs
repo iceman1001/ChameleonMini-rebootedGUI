af7095a1ee031abb08135b160ab43677fe65ed97
iceman1001 <iceman@iuse.se>
2018-12-14 20:58:29 +0100

    chg:  version updates

--- ChameleonMiniGUI/Properties/Settings.Designer.cs
+++ ChameleonMiniGUI/Properties/Settings.Designer.cs
@@ -73,7 +73,7 @@ namespace ChameleonMiniGUI.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("v1.2.0.4")]
+        [global::System.Configuration.DefaultSettingValueAttribute("v1.2.0.7")]
         public string version {
             get {
                 return ((string)(this["version"]));
